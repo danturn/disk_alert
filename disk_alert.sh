a3759301c668f63a95fd73611e6bc03f49bc0ac6
danturn <dan.turner@cloudhouse.com>
2016-10-13 09:45:12 +0100

    using an environment variable for email

--- disk_alert.sh
+++ disk_alert.sh
@@ -1,21 +1,24 @@
-ADMIN="dan.turner@cloudhouse.com"
-# set alert-level 90 % standard
-ALERT=10
+USAGE_THRESHOLD=90
 
-meh() {
+check_disk() {
   local text="The following partitions are low on space:\n"
-
+  local alerts_to_send=false
   while read output
   do
-    usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+    usage=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
     partition=$(echo $output | awk '{ print $2 }' )
-    if [ $usep -ge $ALERT ]; then
-      text="$text\n$usep""% used on $partition"""
+    if [ $usage -ge $USAGE_THRESHOLD ]; then
+      text="$text\n$usage""% used on $partition"""
+      alerts_to_send=true
     fi
-
   done
-  echo -e "$text" |
-  mail -s "Low Disk alert for \"$(hostname)\"" $ADMIN
+
+  if [ $alerts_to_send == true ]
+    then 
+      echo -e "$text" |
+      mail -s "Low Disk alert for \"$(hostname)\"" $ADMIN_EMAIL
+  fi
 }
 
-df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $6 }' | meh
+: "${ADMIN_EMAIL?Need to set the ADMIN_EMAIL environment variable}"
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $6 }' | check_disk
